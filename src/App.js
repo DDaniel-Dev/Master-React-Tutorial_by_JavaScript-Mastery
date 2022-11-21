cf2569287e1120939d1956270af963fae40d1662
DDaniel-Dev <ddaniel.atx@gmail.com>
2022-11-20 22:35:23 -0600

    feat: add Bootstrap 5.2.2 sources to 'public' file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const name = "Daniel D";
+  const isNameShowing = true;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(10)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container bg-dark">
+      <h1 className='text-light'>
+        HELLO REACT and Developer {isNameShowing ? name : "Anonymous!"}!
+      </h1>
+      <button 
+        onClick={() => setCounter((nextCount) => nextCount + 1)}
+      >+
+      </button>
+      <h2 className='text-light'>{counter}</h2>
+      <button
+        onClick={() => setCounter((prevCount) => prevCount - 1)}
+      >-</button>
+      <hr className='text-light'></hr>
+      <h1 className='text-light'>A test text from a recent news...</h1>
+      <br></br>
+      <h3 className='text-light'>
+        FTX has filed for bankruptcy protection in the US after the struggling cryptocurrency exchange failed in a last-ditch effort to secure a rescue package. 
+      Earlier this year, leading voices from the cryptocurrency industry, including FTX's Sam Bankman-Fried and Binance CEO 'CZ', told the story of how the cryptocurrency industry ballooned into a market worth more than $2tn with little regulatory oversight. 
+      How did regulators lose control and can they get a grip now?
+      </h3>
     </div>
   );
 }
